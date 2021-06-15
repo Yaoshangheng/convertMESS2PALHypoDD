6fd1edbc948c9deaf2d160877fcf43a1ca857633
Yuan Yao <yaoyuaneqyn@163.com>
2021-06-15 15:13:36 +0800

    Add files via upload

---
+++ convertMESS2PALHypoDD.py
@@ -0,0 +1,28 @@
+file_path = 'ZSYtmp201701.pha' #要修改的文件路径
+import os
+from sys import argv
+import re
+def main(file_path):
+    num = 1
+    r = re.compile(r'^[0-9]+_[0-9]+')
+    file_path = os.path.abspath(file_path)
+    out_path = os.path.join(os.path.dirname(file_path), "new_" + os.path.basename(file_path)) #修改后的文件路径
+    out_file = open(out_path, "w") #写文件
+    in_file = open(file_path, "r") #读文件
+    for line in in_file:
+        #后面添加数字
+        if r.match(line):
+            out_file.write(line.strip() + "," + str(num) + "\n")
+            num += 1 #数字递增
+        else:
+            out_file.write(line)
+    in_file.close()
+    out_file.close()
+    print("saved to:", out_path)
+
+#从命令行参数读入文件路径
+if len(argv) > 1:
+    file_path = argv[1]
+main(file_path = file_path)
+
+
